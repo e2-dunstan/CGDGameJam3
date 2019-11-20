438330cf340cfdcb2f09c8dc53e075103d6526c2
Lewis <lewis2.bond@live.uwe.ac.uk>
2019-11-20 21:40:14 +0000

    #1 Added in basic lights and positions in the maze

--- CGDGameJam3/Assets/CameraFollow.cs
+++ CGDGameJam3/Assets/CameraFollow.cs
@@ -6,7 +6,8 @@ public class CameraFollow : MonoBehaviour
 {
 
     public GameObject player;
-
+    [SerializeField]
+    Vector3 offset = new Vector3(0.0f, 7.5f, 0.0f);
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,7 @@ public class CameraFollow : MonoBehaviour
     {
         var pos = player.transform.position;
 
-        transform.position = new Vector3(pos.x, pos.y + 10.0f, pos.z);
+        transform.position = new Vector3(pos.x + offset.x, pos.y + offset.y, pos.z + offset.z);
         transform.LookAt(pos, transform.up);
     
     }
---
+++ CGDGameJam3/Assets/TouchLights.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchLights : MonoBehaviour
+{
+
+    public Transform [] potentialPositions;
+    List<bool> positionsTaken = new List<bool>();
+    public GameObject[] lights;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < potentialPositions.Length; ++i)
+        {
+            positionsTaken.Add(false);
+        }
+
+        for (int i = 0; i < lights.Length; ++i)
+        {
+            int ranPos = Random.Range(0, potentialPositions.Length);
+
+            while (positionsTaken[ranPos] == true)
+            {
+                ranPos = Random.Range(0, potentialPositions.Length);
+            }
+
+            lights[i].transform.position = potentialPositions[ranPos].transform.position;
+            positionsTaken[ranPos] = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
