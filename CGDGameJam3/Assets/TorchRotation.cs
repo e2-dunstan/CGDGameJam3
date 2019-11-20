bec9cbcd75003e43bc7847f6cd7b9af5193ffe85
Ben Green <ben11344@msn.com>
2019-11-20 17:40:54 +0000

    LightyBoi Is Lighting

---
+++ CGDGameJam3/Assets/TorchRotation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchRotation : MonoBehaviour
+{ 
+
+    InputHandler inputHandler = InputHandler.Instance();
+    public float moveSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateToMouse();
+    }
+
+    private void rotateToMouse()
+    {
+        var playerPlane = new Plane(Vector3.up, transform.position);
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        float hitdist = 0.0f;
+
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            var targetPoint = ray.GetPoint(hitdist);
+
+            var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, moveSpeed * Time.deltaTime);
+        }
+    }
+}
