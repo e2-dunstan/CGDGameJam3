8426602ac3f56321e6801d44bdeb3c0c9c0c2ea0
Ellie <ellie2.dunstan@live.uwe.ac.uk>
2019-11-22 13:54:15 +0000

    #11 menu, with creepy effects

---
+++ CGDGameJam3/Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    private Light lightComponent;
+    private float baseIntensity;
+
+    public float amount = 0.5f;
+    public float speed = 1.0f;
+
+    private float timeElapsed = 0;
+    private float timeBetweenFlickers = 0.1f;
+
+    private void Start()
+    {
+        lightComponent = GetComponent<Light>();
+        baseIntensity = lightComponent.intensity;
+    }
+
+    private void Update()
+    {
+        if (timeElapsed >= timeBetweenFlickers)
+        {
+            StartCoroutine(AdjustIntensity());
+            timeElapsed = 0;
+            timeBetweenFlickers = Random.Range(0.05f, 0.1f);
+        }
+        else
+        {
+            timeElapsed += Time.deltaTime * speed;
+        }
+    }
+
+    private IEnumerator AdjustIntensity()
+    {
+        float newIntensity = baseIntensity + Random.Range(-amount, amount);
+        float currentIntensity = lightComponent.intensity;
+        float diff = newIntensity - currentIntensity;
+
+        for(float t = 0.0001f; t < 1; t += Time.deltaTime * 10f)
+        {
+            lightComponent.intensity = currentIntensity + (diff * t / 1.0f);
+            yield return null;
+        }
+    }
+}
---
+++ CGDGameJam3/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Text title;
+    private Color defaultTitleColour;
+    private Vector3 defaultTitlePos;
+
+    private float timeElapsed = 0;
+    private float flashDelay = 1;
+
+
+    private void Start()
+    {
+        defaultTitleColour = title.color;
+        defaultTitlePos = title.transform.position;
+    }
+
+
+    private void Update()
+    {
+        if (timeElapsed >= flashDelay)
+        {
+            StartCoroutine(GlitchTitle());
+
+            timeElapsed = 0;
+            flashDelay = Random.Range(1f, 5f);
+        }
+        else timeElapsed += Time.deltaTime;
+    }
+
+    private IEnumerator GlitchTitle()
+    {
+        int flashCount = Random.Range(1, 4);
+        for (int i = 0; i < flashCount; i++)
+        {
+            title.color = Color.white;
+            title.transform.position += new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), 0);
+
+            yield return new WaitForSeconds(Random.Range(0.08f, 0.15f));
+
+            title.color = defaultTitleColour;
+            title.transform.position = defaultTitlePos;
+
+            yield return new WaitForSeconds(Random.Range(0.08f, 0.15f));
+        }
+    }
+}
