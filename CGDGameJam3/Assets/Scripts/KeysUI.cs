692e4170f934c7ada78523c2f0c8706b57ca5f87
Sol Morrison <solomon2.morrison@live.uwe.ac.uk>
2019-11-24 18:52:09 +0000

    #6 Added TMPro, added canvas & placeholder Key images

---
+++ CGDGameJam3/Assets/Scripts/KeysUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeysUI : MonoBehaviour
+{
+    public Image[] keyImages;
+
+
+    private void Awake()
+    {
+        keyImages = GetComponentsInChildren<Image>();
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Image key in keyImages)
+            key.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
