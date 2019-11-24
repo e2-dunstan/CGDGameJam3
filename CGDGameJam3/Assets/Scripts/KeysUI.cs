12ab29ed8dc31bd6b50c755b951d0800281e5f80
Sol Morrison <solomon2.morrison@live.uwe.ac.uk>
2019-11-24 18:58:30 +0000

    #6 Added bool to trigger Key image popup

--- CGDGameJam3/Assets/Scripts/KeysUI.cs
+++ CGDGameJam3/Assets/Scripts/KeysUI.cs
@@ -7,7 +7,9 @@ public class KeysUI : MonoBehaviour
 {
     public Image[] keyImages;
 
-
+    public bool key1Held;
+    public bool key2Held;
+    public bool key3Held;
     private void Awake()
     {
         keyImages = GetComponentsInChildren<Image>();
@@ -24,6 +26,11 @@ public class KeysUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (key1Held)
+            keyImages[0].enabled = true;
+        if (key2Held)
+            keyImages[1].enabled = true;
+        if (key3Held)
+            keyImages[2].enabled = true;
     }
 }
