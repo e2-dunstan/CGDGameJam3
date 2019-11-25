c901081de641381e89f7a48f43d30ccdc19215aa
Alex Allman <xalexxd@hotmail.co.uk>
2019-11-25 14:59:59 +0000

    #7 Base system in place

---
+++ CGDGameJam3/Assets/Scripts/Keys/KeyManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyManager : MonoBehaviour
+{
+    [Header("Object References")]
+    public KeyPickup[] keys;
+    public LightPost[] lightPosts;
+
+
+    private int keysUncollected;
+
+    void Start()
+    {
+        foreach (var key in keys)
+        {
+            key.SetManagerReference(this);
+        }
+
+        foreach (var light in lightPosts)
+        {
+            light.SetLightActive(false);
+        }
+
+        keysUncollected = keys.Length;
+    }
+
+    public void KeyCollected(KeyPickup _key)
+    {
+        for (int i = 0; i < keys.Length; i++)
+        {
+            if(_key == keys[i])
+            {
+                _key.KeyActive(false);
+                lightPosts[i].SetLightActive(true);
+                keysUncollected--;
+
+                if(keysUncollected == 0)
+                {
+                    AllKeysCollected();
+                }
+            }
+        }
+    }
+
+    private void AllKeysCollected()
+    {
+        Debug.Log("All keys collected. Do something here to finish the game");
+    }
+}
