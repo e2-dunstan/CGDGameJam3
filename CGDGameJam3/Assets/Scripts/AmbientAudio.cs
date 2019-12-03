0885f0dc482cf32144065d9350f3d3d544807115
Sol Morrison <solomon2.morrison@live.uwe.ac.uk>
2019-12-03 17:31:34 +0000

    Random ambient wind sound playing (it doesn't turn off on death)

---
+++ CGDGameJam3/Assets/Scripts/AmbientAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientAudio : MonoBehaviour
+{
+    FMOD.Studio.EventInstance windEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        windEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(PlayerManager.Instance().players[0].gameObject));
+
+        if (!isPlaying(windEvent))
+        {
+            string windSound = "event:/Ambience/Wind";
+            var windRandomValue = Random.Range(1, 3);
+            windSound = windSound.Insert(windSound.Length, windRandomValue.ToString());
+            windEvent = FMODUnity.RuntimeManager.CreateInstance(windSound);
+            windEvent.start();
+            print(windEvent +windSound);
+
+        }
+    }
+
+    bool isPlaying(FMOD.Studio.EventInstance _event)
+    {
+        FMOD.Studio.PLAYBACK_STATE playbackState;
+        _event.getPlaybackState(out playbackState);
+
+        return playbackState != FMOD.Studio.PLAYBACK_STATE.STOPPED;
+    }
+}
